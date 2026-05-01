7c8294c65a9c59c53757f5518075cc1a439e7cbf
williamdouglasmsouza <williamdouglasmsouza@gmail.com>
2026-05-01 19:51:25 -0300

    Initial commit - Multiplayer PvE Survival 3D (with .gitignore)

--- server/dist/database/db.d.ts
+++
@@ -1,6 +0,0 @@
-import { Pool } from 'pg';
-export declare function initDatabase(): Pool;
-export declare function getPool(): Pool;
-export declare function closeDatabase(): Promise<void>;
-export declare function testConnection(): Promise<boolean>;
-//# sourceMappingURL=db.d.ts.map
--- server/dist/network/Protocol.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Network protocol - all message types between client and server
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=Protocol.js.map
